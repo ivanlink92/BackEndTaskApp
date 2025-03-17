ecda085ba979c6000a6554507a648964945533ea
Ivan Lingga <ivan_link92@hotmail.com>
2025-03-18 03:04:04 +1300

    Editing Logout Done

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LoginView, RegisterView, TaskViewSet
+from .views import LoginView, LogoutView, RegisterView, TaskViewSet
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet,basename='task')
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
 ]
